627fddab9f37b1e56ae2ee9e43a98ec13fafd1a7
aitorp28 <62764832+aitorp28@users.noreply.github.com>
2021-09-20 14:23:17 +0200

    20-09-2021
    Creados archivos necesarios

---
+++ src/holamundomvc/controller/View.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundomvc.controller;
+
+/**
+ *Esta interfaz contiene las funciones que se implementan a la vista.
+ * @author aitor
+ */
+public interface View {
+    /**
+     * 
+     * @param greeting Muestra un saludo tipo texto
+     */
+    public  void showGreeting(String greeting);
+}
